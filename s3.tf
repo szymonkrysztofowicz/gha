f1bfd50fa2c0b602c66fe2f8c2dc2b5a04af4b83
szymonkrysztofowicz <108467650+szymonkrysztofowicz@users.noreply.github.com>
2022-08-12 13:20:15 +0200

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "sk-terraform-2022-state-backend"
+  object_lock_enabled = true
+  tags = {
+    Name = "S3 Remote Terraform State Store"
+  }  
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+ bucket = aws_s3_bucket.bucket.id
+ versioning_configuration {
+   status = "Enabled"
+ }
+}
