25d2a51e2b01b5c0c2505cc297825f5570541235
szymonkrysztofowicz <108467650+szymonkrysztofowicz@users.noreply.github.com>
2022-08-19 14:23:42 +0200

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket          = "sk-terraform-2022-state-backend"
-        encrypt         = true
-        key             = "terraform-state.tfstate"
-        region          = "us-east-1"
-    }
-}
