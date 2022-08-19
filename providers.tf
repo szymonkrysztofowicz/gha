42c907982ec1ab50a09ddcfc77472a02a1137961
szymonkrysztofowicz <108467650+szymonkrysztofowicz@users.noreply.github.com>
2022-08-19 13:59:12 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 terraform {
-    required_version = "~> 1.2.3"
+#     required_version = "~> 1.2.3"
     required_providers {
       aws = {
         source  = "hashicorp/aws"
