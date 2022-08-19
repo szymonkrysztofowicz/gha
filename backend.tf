d658eb9ee159e73c0acbbc80c6730875cf0ee107
szymonkrysztofowicz <108467650+szymonkrysztofowicz@users.noreply.github.com>
2022-08-19 14:19:23 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket          = "arn:aws:s3:::sk-terraform-2022-state-backend"
+        bucket          = "sk-terraform-2022-state-backend"
         encrypt         = true
         key             = "terraform-state.tfstate"
         region          = "us-east-1"
